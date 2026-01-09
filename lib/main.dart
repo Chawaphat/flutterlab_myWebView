c18668b56d5ccfa3110e835eac2829ce5fd849e0
monfufuu <ica.chawaphat@gmail.com>
2026-01-09 10:58:46 +0700

    addNavgaitor

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,9 @@ class MyWebView extends StatefulWidget {
 
 class _MyWebViewState extends State<MyWebView> {
   late final WebViewController controller;
+  late final WebViewController controller1;
+  late final WebViewController controller2;
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -34,6 +37,37 @@ class _MyWebViewState extends State<MyWebView> {
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..loadFlutterAsset('assets/index.html');
+
+    controller1 = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadHtmlString(
+        "<html><head><title>Local HTML String</title></head><body><h1>This is a local HTML string</h1><p>Hello from the local HTML string!</p></body></html>",
+      );
+
+    controller2 = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            setState(() {
+              isLoading = true;
+            });
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://flutter.dev') ||
+                request.url.startsWith('https://docs.flutter.dev')) {
+              return NavigationDecision.navigate;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://flutter.dev'));
   }
 
   @override
@@ -47,6 +81,8 @@ class _MyWebViewState extends State<MyWebView> {
             onPressed: () async {
               if (await controller.canGoBack()) {
                 controller.goBack();
+                controller1.goBack();
+                controller2.goBack();
               }
             },
           ),
@@ -55,6 +91,8 @@ class _MyWebViewState extends State<MyWebView> {
             onPressed: () async {
               if (await controller.canGoForward()) {
                 controller.goForward();
+                controller1.goForward();
+                controller2.goForward();
               }
             },
           ),
@@ -62,11 +100,27 @@ class _MyWebViewState extends State<MyWebView> {
             icon: const Icon(Icons.refresh),
             onPressed: () {
               controller.reload();
+              controller1.reload();
+              controller2.reload();
             },
           ),
         ],
       ),
-      body: WebViewWidget(controller: controller),
+      body: Column(
+        children: [
+          Expanded(child: WebViewWidget(controller: controller)),
+          const Divider(height: 1),
+          Expanded(child: WebViewWidget(controller: controller1)),
+          const Divider(height: 1),
+          Expanded(child: WebViewWidget(controller: controller2)),
+
+          if (isLoading)
+            Container(
+              color: Colors.black26,
+              child: const Center(child: CircularProgressIndicator()),
+            ),
+        ],
+      ),
     );
   }
 }
